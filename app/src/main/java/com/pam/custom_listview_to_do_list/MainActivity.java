8f58b65dd2b1669daaa0eef74463e5095f5478be
pamsfields <jh5811zj@go.minneapolis.edu>
2017-09-30 13:40:24 -0500

    Rework of app

--- app/src/main/java/com/pam/custom_listview_to_do_list/MainActivity.java
+++ app/src/main/java/com/pam/custom_listview_to_do_list/MainActivity.java
@@ -8,8 +8,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.Date;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
